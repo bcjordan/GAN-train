24c7448614f9f761d09850c1db02588a696ff8b6
Brian Jordan <bcjordan@gmail.com>
2019-10-13 02:53:44 -0400

    Project skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import logo from './logo512.png';
+import './App.css';
+
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+
+class ColoredRect extends React.Component {
+  state = {
+    color: 'green'
+  };
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+  render() {
+    return (
+        <Rect
+            x={this.props.x}
+            y={20}
+            width={50}
+            height={50}
+            fill={this.state.color}
+            shadowBlur={5}
+            onClick={this.handleClick}
+        />
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}/>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <Text text="Try click on rect" />
+            <ColoredRect x={5}  />
+            <ColoredRect x={10}/>
+            <ColoredRect x={15}/>
+          </Layer>
+        </Stage>
+      </header>
+    </div>
+  );
+}
+
+export default App;
