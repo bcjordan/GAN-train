bbcccc88b50e96bdb52bdb92e7e3f9fce5aba27d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-13 19:45:54 +0000

    Deploy bcjordan/GAN-train to github.com/bcjordan/GAN-train.git:gh-pages

---
+++ precache-manifest.bb1257bbefc48525350af0bed26925d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e49265bebd0bcd2c8a01f72965ecab2",
+    "url": "/GAN-train/index.html"
+  },
+  {
+    "revision": "f69ba4def6d2bea56811",
+    "url": "/GAN-train/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fb1ec6bc835e7ec15a0f",
+    "url": "/GAN-train/static/js/2.3c2a93af.chunk.js"
+  },
+  {
+    "revision": "f69ba4def6d2bea56811",
+    "url": "/GAN-train/static/js/main.ad08fee6.chunk.js"
+  },
+  {
+    "revision": "6a628809f62aa0fda312",
+    "url": "/GAN-train/static/js/runtime-main.622c0e13.js"
+  },
+  {
+    "revision": "260d57586012b0ed1ae78accc0bf7083",
+    "url": "/GAN-train/static/media/logo512.260d5758.png"
+  }
+]);
